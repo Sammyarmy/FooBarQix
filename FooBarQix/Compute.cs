44c8160b5baad172c9e66c1318a371c3ce8107df
Sam Armstrong <Sam.Armstrong@sorted.com>
2023-02-28 14:26:33 +0000

    added gitignores

---
+++ FooBarQix/Compute.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBarQix
+{
+    public class Compute
+    {
+        public string compute(string input)
+        {
+            int intInput = int.Parse(input);
+            string output = "";
+            bool isTriggered = false;
+
+            if (intInput % 3 == 0)
+            {
+                isTriggered = true;
+                output = string.Concat(output, "Foo");
+            }
+            if (intInput % 5 == 0)
+            {
+                isTriggered = true;
+                output = string.Concat(output, "Bar");
+            }
+            if (intInput % 7 == 0)
+            {
+                isTriggered = true;
+                output = string.Concat(output, "Qix");
+            }
+
+            foreach (var item in input)
+            {
+                if (item == '3')
+                {
+                    output = string.Concat(output, "Foo");
+                    isTriggered = true;
+                }
+                if (item == '5')
+                {
+                    output = string.Concat(output, "Bar");
+                    isTriggered = true;
+                }
+                if (item == '7')
+                {
+                    output = string.Concat(output, "Qix");
+                    isTriggered = true;
+                }
+            }
+
+            if (!isTriggered)
+            {
+                output = string.Concat(output, input);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ FooBarQix/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace FooBarQix
+{
+    public class Tests
+    {
+        [Test]
+        [TestCase("33", "FooFooFoo")]
+        [TestCase("15", "FooBarBar")]
+        [TestCase("8", "8")]
+        [TestCase("21", "FooQix")]
+        public void testCompute(string input, string output)
+        {
+            Compute compute = new Compute();
+            Assert.AreEqual(compute.compute(input), output);
+        }
+
+    }
+}
