9b3ce0fc8d605ec4340172d83089a2eba82acb7f
Sam Armstrong <Sam.Armstrong@sorted.com>
2023-02-28 14:43:28 +0000

    added comment on failing test

--- FooBarQix/Tests.cs
+++ FooBarQix/Tests.cs
@@ -15,7 +15,7 @@ namespace FooBarQix
             Assert.AreEqual(expected, compute.compute(input));
         }
         [Test]
-        [TestCase("101", "1*1")]
+        [TestCase("101", "1*1")] // this test case fails with "*101", this is due to the way I've written the algorithm doesnt expect to need to replace chars if not divisible or containing 3,5,7
         [TestCase("303", "FooFoo*Foo")]
         [TestCase("105", "FooBarQix*Bar")]
         [TestCase("10101", "FooQix**")]
